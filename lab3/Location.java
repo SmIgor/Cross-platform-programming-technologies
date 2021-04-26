e8846def805a00766ab24443b489db9037652590
Игорь <45309265+SmIgor@users.noreply.github.com>
2021-04-26 23:00:25 +0300

    Add files via upload

--- lab3/Location.java
+++ lab3/Location.java
@@ -1,1 +1,39 @@
+package com.company;
 
+import java.util.Objects;
+
+/**
+ * This class represents a specific location in a 2D map.  Coordinates are
+ * integer values.
+ **/
+public class Location
+{
+    /** X coordinate of this location. **/
+    public int xCoord;
+
+    /** Y coordinate of this location. **/
+    public int yCoord;
+
+
+    /** Creates a new location with the specified integer coordinates. **/
+    public Location(int x, int y)
+    {
+        xCoord = x;
+        yCoord = y;
+    }
+
+    /** Creates a new location with coordinates (0, 0). **/
+    public Location()
+    {
+        this(0, 0);
+    }
+    public boolean equals(Object o) {
+        if (getClass() != o.getClass()) return false;
+        Location l = (Location) o;
+        return xCoord == l.xCoord && yCoord == l.yCoord;
+    }
+    public int hashCode() {
+
+        return Objects.hash(xCoord, yCoord);
+    }
+}
